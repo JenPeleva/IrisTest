6f380bef74641a55ce712c1892db358fdc46bd76
Jen Peleva <zheyna.peleva@telerik.com>
2017-07-13 15:25:04 +0300

    framework files

---
+++ modules/sass.js
@@ -0,0 +1,37 @@
+/**
+ * Created by peleva on 7/13/2017.
+ */
+'use strict';
+
+var NOAH = NOAH || {};
+
+/**
+ * NOAH.sass
+ * Compile Sass/Scss files
+ */
+NOAH.sass = (function(options) {
+
+    // Requirements
+    var sass = require('node-sass');
+    var fs = require('fs');
+    var mkdirp = require('mkdirp');
+    var getDirName = require('path').dirname;
+
+    // Options
+    var style = options.style || 'expanded';
+
+    var result = sass.renderSync({
+        file: options.src,
+        outputStyle: style
+    });
+
+    mkdirp(getDirName(options.dest), function(err) {
+        if (err) return cb(err);
+        fs.writeFile(options.dest, result.css);
+    });
+
+    console.log(' ' + options.dest + ' built.');
+});
+
+// Export the function to use in other files
+exports.NOAH = NOAH.sass;
--- vendor/test.md
+++
@@ -1,2 +0,0 @@
-
-Jeina peleva test 12345
